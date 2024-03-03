74c1b2df404319ef03749b5fc20613cc0144da1a
Janina Viitanen <janinamargit.viitanen@gmail.com>
2024-03-03 16:35:55 +0200

    3.18 done

--- index.js
+++ index.js
@@ -16,33 +16,12 @@ app.use(cors())
 
 app.use(express.static('dist'))
 
-let persons = [
-    {
-        id: 1,
-        name: "Arto Hellas",
-        number: "040-123456"
-    },
-    {
-        id: 2,
-        name: "Ada Lovelace",
-        number: "39-44-5323523"
-    },
-    {
-        id: 3,
-        name: "Dan Abramov",
-        number: "12-43-234345"
-    },
-    {
-        id: 4,
-        name: "Mary Poppendick",
-        number: "39-23-6423122"
-    }
-]
 
 app.get('/info', (request, response) => {
-    response.send(`<p>Phonebook has info for ${persons.length} people</p>
-                <p>${new Date()}</p>`)
-    
+    Person.countDocuments().then(p => {
+        response.send(`<p>Phonebook has info for ${p} people</p>
+                        <p>${new Date()}</p>`)
+    })    
 })
 
 app.get('/api/persons', (request, response) => {
@@ -89,11 +68,7 @@ app.post('/api/persons', (request, response) => {
         return response.status(400).json({
             error: 'number missing'
         })
-    } /*else if (persons.find(p => p.name === body.name)) {
-        return response.status(400).json({
-            error: 'name must be unique'
-        })
-    }*/
+    }
 
     const person = new Person({
         name: body.name,
@@ -105,6 +80,21 @@ app.post('/api/persons', (request, response) => {
             response.json(savedPerson)
         })
     
+})
+
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
+
+    const new_info = {
+        name: body.name,
+        number: body.number
+    }
+    
+    Person.findByIdAndUpdate(request.params.id, new_info, {new: true})
+        .then(updatedPerson => {
+            response.json(updatedPerson)
+        })
+        .catch(error => next(error))
 })
 
 const errorHandler = (error, request, response, next) => {
